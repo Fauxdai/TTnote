0e5eeb1dd357aa03b7e4c8d556535ad9e29c632c
Foxmeadow <38027739+Fauxdai@users.noreply.github.com>
2024-02-20 22:58:20 -0500

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,67 +1,81 @@
 import os
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+
+colorama_init()
 
 # Dictionary to store notes
-notes = {'People': {}, 'Places': {}, 'Things that Happen': {}, 'Items': {}, 'History': {}, 'Knowledge': {},'Meta':{}, 'Jokes': {}}
+notes = {'People': {}, 'Places': {}, 'Things that Happen': {}, 'Items': {}, 'History': {}, 'Knowledge': {}, 'Jokes': {}, 'Meta':{}}
 
 # Function to display the last 5 logs
 def display_last_logs():
     for category, entries in notes.items():
         if entries:
-            print(f"\nCategory: {category}")
-            print('#' * 60 + '\n')
-            for title, description in list(entries.items())[-50:]:
-                print(f"Title: {title}")
-                print(f"Description: {description}\n")
-                print('-' * 30)
+            print(Fore.LIGHTMAGENTA_EX + f"---------- Category: {category} ----------")
+            for title, description in list(entries.items())[-5:]:
+                print(Fore.LIGHTGREEN_EX + f"-Title: {title.capitalize()}")
+                print(Fore.LIGHTMAGENTA_EX + f"--Description: \n\n - {description}\n")
+                print(Fore.LIGHTMAGENTA_EX + '---' * 30)
 
 # Function to save notes to a text file
 def save_notes(session_num):
     with open(f"session_{session_num}_notes.txt", 'w') as file:
         for category, entries in notes.items():
-            file.write(f"\nCategory: {category}\n")
-            file.write('#' * 60 + '\n')
             for title, description in entries.items():
-                file.write(f"Title: {title}\n")
-                file.write(f"Description: {description}\n")
-                file.write('-' * 30 + '\n')
-                
-                
+                file.write(f"---------- Category: {category} ---------\n")
+                file.write(f"-Title: {title.capitalize()}\n")
+                file.write(f"--Description:\n\n {description}\n")
+                file.write('---' * 30 + '\n')
+
+def displayBanner():
+    banner = f"""
+|------------------ WELCOME TO THE TTRPG Notatilator V.1! ------------------|
+|Choose a category below, Write a title, Then write a description for entry!|
+|To append an entry, simply duplicate your title")                          |
+|---------------------------------------------------------------------------|
+    """
+    print(Fore.CYAN + banner)
+
+def displayChoices():
+    choice = f"""
+Choose a category:
+1. People
+2. Places
+3. Things that Happen
+4. Items
+5. History
+6. Knowledge
+7. Jokes
+8. Meta/OOC
+9. Exit
+    """
+    print(Fore.GREEN + choice)
+
 # Main menu loop
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the terminal screen
-    print("------------------ WELCOME TO THE TTRPG Notatilator V.1! ------------------")
-    print("Choose a category below, Write a title, Then write a description for entry!")
-    print("To append an entry, simply duplicate your title")
-    print("---------------------------------------------------------------------------")
+    displayBanner()
     display_last_logs()
-    print("\nChoose a category:")
-    print("1. People")
-    print("2. Places")
-    print("3. Things that Happen")
-    print("4. Items")
-    print("5. History")
-    print("6. Knowledge")
-    print("7. Meta")
-    print("8. Jokes")
-    print("9. Exit")
+    displayChoices()
 
     choice = input("Enter your choice: ")
 
     if choice == '9':
-        session_num = input("Name of File: ")
+        session_num = input("Enter session name: ")
         save_notes(session_num)
         break
 
     if choice in {'1', '2', '3', '4', '5', '6', '7', '8'}:
         category = list(notes.keys())[int(choice) - 1]
-        title = input("Enter a title: ")
+        title = input("  Enter a title: ")
 
-        if title.casefold().title() in notes[category]:
-            description = input("Enter a description (append to the existing entry): ")
-            notes[category][title.casefold().title()] += '\n' + '\n' + "- " + description
+        if title.casefold() in notes[category]:
+            description = input("    Enter a description (append to the existing entry): ")
+            notes[category][title.casefold()] += '\n' + '\n' + "- " + description
         else:
-            description = input("Enter a description: ")
-            notes[category][title.casefold().title()] = '\n'+ "- " + description
+            description = input("    Enter a description: ")
+            notes[category][title.casefold()] = description
 
 # End of the program
 
